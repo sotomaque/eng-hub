86ab53de5739b58f20d57951d6dca5fba0b18dd1
Enrique Sotomayor <sotomaque@gmail.com>
2026-02-21 17:47:03 -0800

    chore: rename env variables per supabase integration standards

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    POSTGRES_PRISMA_URL: z.string().url(),
     CLERK_SECRET_KEY: z.string(),
     UPLOADTHING_TOKEN: z.string().optional(),
     GITHUB_TOKEN: z.string().optional(),
@@ -21,7 +21,7 @@ export const env = createEnv({
   },
 
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
