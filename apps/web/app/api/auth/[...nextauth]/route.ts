d3d960ed2a50bc79c49a7b7eb1dfc26834149cd9
Enrique Sotomayor <sotomaque@gmail.com>
2026-04-17 09:20:50 -0700

    fix: typecheck

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
 import { handlers } from "@workspace/auth/entra-config";
 
-export const { GET, POST } = handlers;
+export const GET: (typeof handlers)["GET"] = handlers.GET;
+export const POST: (typeof handlers)["POST"] = handlers.POST;
